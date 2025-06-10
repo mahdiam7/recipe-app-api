66c61626c1532455ade86f0760ea372f46c0c715
Mahdi <cr7.mahdi.ma.ma@gmail.com>
2025-06-10 10:34:55 +0330

    Sample of SimpleTestCase

--- app/app/test.py
+++ app/app/test.py
@@ -11,6 +11,6 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding nubmers together"""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
